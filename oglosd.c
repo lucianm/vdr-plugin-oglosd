1116f0413ced36daa8fb1e0f088b6cc708f6221e
Lucian Muresan <lucianm@users.sourceforge.net>
2016-04-28 00:10:32 +0200

    first additions to README and non-code adaptations to oglosd.c

--- oglosd.c
+++ oglosd.c
@@ -9,15 +9,15 @@
 #include <vdr/plugin.h>
 
 static const char *VERSION        = "0.0.1";
-static const char *DESCRIPTION    = "Enter description for 'oglosd' plugin";
-static const char *MAINMENUENTRY  = "Oglosd";
+static const char *DESCRIPTION    = "Generic OpenGL/ES OSD provider";
+static const char *MAINMENUENTRY  = "OpenGL OSD";
 
-class cPluginOglosd : public cPlugin {
+class cPluginOglOsd : public cPlugin {
 private:
   // Add any member variables or functions you may need here.
 public:
-  cPluginOglosd(void);
-  virtual ~cPluginOglosd();
+  cPluginOglOsd(void);
+  virtual ~cPluginOglOsd();
   virtual const char *Version(void) { return VERSION; }
   virtual const char *Description(void) { return DESCRIPTION; }
   virtual const char *CommandLineHelp(void);
@@ -38,104 +38,104 @@ public:
   virtual cString SVDRPCommand(const char *Command, const char *Option, int &ReplyCode);
   };
 
-cPluginOglosd::cPluginOglosd(void)
+cPluginOglOsd::cPluginOglOsd(void)
 {
   // Initialize any member variables here.
   // DON'T DO ANYTHING ELSE THAT MAY HAVE SIDE EFFECTS, REQUIRE GLOBAL
   // VDR OBJECTS TO EXIST OR PRODUCE ANY OUTPUT!
 }
 
-cPluginOglosd::~cPluginOglosd()
+cPluginOglOsd::~cPluginOglOsd()
 {
   // Clean up after yourself!
 }
 
-const char *cPluginOglosd::CommandLineHelp(void)
+const char *cPluginOglOsd::CommandLineHelp(void)
 {
   // Return a string that describes all known command line options.
   return NULL;
 }
 
-bool cPluginOglosd::ProcessArgs(int argc, char *argv[])
+bool cPluginOglOsd::ProcessArgs(int argc, char *argv[])
 {
   // Implement command line argument processing here if applicable.
   return true;
 }
 
-bool cPluginOglosd::Initialize(void)
+bool cPluginOglOsd::Initialize(void)
 {
   // Initialize any background activities the plugin shall perform.
   return true;
 }
 
-bool cPluginOglosd::Start(void)
+bool cPluginOglOsd::Start(void)
 {
   // Start any background activities the plugin shall perform.
   return true;
 }
 
-void cPluginOglosd::Stop(void)
+void cPluginOglOsd::Stop(void)
 {
   // Stop any background activities the plugin is performing.
 }
 
-void cPluginOglosd::Housekeeping(void)
+void cPluginOglOsd::Housekeeping(void)
 {
   // Perform any cleanup or other regular tasks.
 }
 
-void cPluginOglosd::MainThreadHook(void)
+void cPluginOglOsd::MainThreadHook(void)
 {
   // Perform actions in the context of the main program thread.
   // WARNING: Use with great care - see PLUGINS.html!
 }
 
-cString cPluginOglosd::Active(void)
+cString cPluginOglOsd::Active(void)
 {
   // Return a message string if shutdown should be postponed
   return NULL;
 }
 
-time_t cPluginOglosd::WakeupTime(void)
+time_t cPluginOglOsd::WakeupTime(void)
 {
   // Return custom wakeup time for shutdown script
   return 0;
 }
 
-cOsdObject *cPluginOglosd::MainMenuAction(void)
+cOsdObject *cPluginOglOsd::MainMenuAction(void)
 {
   // Perform the action when selected from the main VDR menu.
   return NULL;
 }
 
-cMenuSetupPage *cPluginOglosd::SetupMenu(void)
+cMenuSetupPage *cPluginOglOsd::SetupMenu(void)
 {
   // Return a setup menu in case the plugin supports one.
   return NULL;
 }
 
-bool cPluginOglosd::SetupParse(const char *Name, const char *Value)
+bool cPluginOglOsd::SetupParse(const char *Name, const char *Value)
 {
   // Parse your own setup parameters and store their values.
   return false;
 }
 
-bool cPluginOglosd::Service(const char *Id, void *Data)
+bool cPluginOglOsd::Service(const char *Id, void *Data)
 {
   // Handle custom service requests from other plugins
   return false;
 }
 
-const char **cPluginOglosd::SVDRPHelpPages(void)
+const char **cPluginOglOsd::SVDRPHelpPages(void)
 {
   // Return help text for SVDRP commands this plugin implements
   return NULL;
 }
 
-cString cPluginOglosd::SVDRPCommand(const char *Command, const char *Option, int &ReplyCode)
+cString cPluginOglOsd::SVDRPCommand(const char *Command, const char *Option, int &ReplyCode)
 {
   // Process SVDRP commands this plugin implements
   return NULL;
 }
 
-VDRPLUGINCREATOR(cPluginOglosd); // Don't touch this!
+VDRPLUGINCREATOR(cPluginOglOsd); // Don't touch this!
